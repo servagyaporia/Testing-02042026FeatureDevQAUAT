35a18c1eed599b16635db89245b0d9905d35742a
Poria-star <servagya.poria@gmail.com>
2026-04-04 02:32:47 +0530

    j

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -55,7 +55,7 @@ namespace Retail_Web_DFA.Controllers
         public void IPL()
         {
 
-            String a = "India  Bharat";
+            String a = "India  Bharat Devportal";
             Console.WriteLine(a);
         }
 
