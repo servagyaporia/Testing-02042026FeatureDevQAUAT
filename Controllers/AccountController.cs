23cd4a605e256494d504f282e67e7afc199b0c66
Poria-star <servagya.poria@gmail.com>
2026-04-04 01:10:36 +0530

    Commit class and mul

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -40,5 +40,11 @@ namespace Retail_Web_DFA.Controllers
             int a = 20 - 5;
             Console.WriteLine(a);
         }
+
+        public void Mul()
+        {
+            int a = 20 * 5;
+            Console.WriteLine(a);
+        }
     }
 }
---
+++ Models/Class2.cs
@@ -0,0 +1,9 @@
+﻿namespace Retail_Web_DFA.Models
+{
+    public class Class2
+    {
+        public int id { get; set; }
+        public string name { get; set; }    
+        public string description { get; set; }
+    }
+}
