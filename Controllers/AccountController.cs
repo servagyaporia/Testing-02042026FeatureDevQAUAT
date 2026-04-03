92f8d0f179dfcdde596ae62c8e0f358e28c636e8
Poria-star <servagya.poria@gmail.com>
2026-04-04 01:30:47 +0530

    a

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -34,11 +34,11 @@ namespace Retail_Web_DFA.Controllers
 
         public void feature2()
         {
-            Console.WriteLine("Feature 2");
+            Console.WriteLine("Feature two");
         }
         public void add()
         {
-            int a = 10 + 20 + 30;
+            int a = 10 + 30 + 30;
             Console.WriteLine(a);
         }
     }
